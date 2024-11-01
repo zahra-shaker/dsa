436af8375aa140fab407c7dee2ac2ecb8b2b530c
zahra-shaker <zshaker2005@gmail.com>
2024-11-01 23:53:06 +0330

    adding "addition function for big numbers"

--- big number project/bigNumber.cpp
+++ big number project/bigNumber.cpp
@@ -9,7 +9,18 @@ class bigNumber {
       number="0" ; }
     bigNumber(const string& num) {
       number = num; }
-    bigNumber add (const bigNumber&);
+    bigNumber add (const bigNumber& numOther) {
+      string result="0";
+      int lnght=number.lenght();
+      if(numOther.number.lenght() > lnght) 
+        lnght=numOther.number.lenght();
+      for(int i=lnght-1; i>-1 ; i--) {
+        result[i] = (number[i] + numOther[i])%10 ;
+        result[i-1] += (number[i] + numOther[i])/10 ; }
+        return result;
+    }
+        
+      
     bigNumber subtract (const bigNumber&);
     friend bigNumber operator<< (bigNumber);
     friend bigNumber operator>> (bigNumber);
