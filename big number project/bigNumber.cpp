465b35b31cd1530865b27eec714b529ada337ce5
zahra-shaker <zshaker2005@gmail.com>
2024-11-02 00:13:59 +0330

    adding "subtraction function for big numbers"

--- big number project/bigNumber.cpp
+++ big number project/bigNumber.cpp
@@ -11,17 +11,25 @@ class bigNumber {
       number = num; }
     bigNumber add (const bigNumber& numOther) {
       string result="0";
-      int lnght=number.lenght();
-      if(numOther.number.lenght() > lnght) 
-        lnght=numOther.number.lenght();
-      for(int i=lnght-1; i>-1 ; i--) {
-        result[i] = (number[i] + numOther[i])%10 ;
-        result[i-1] += (number[i] + numOther[i])/10 ; }
+      int lngth=number.length();
+      if(numOther.number.length() > lngth) 
+        lngth=numOther.number.length();
+      for(int i=lngth-1; i>-1 ; i--) {
+        result[i] = (number[i] + numOther.number[i])%10 ;
+        result[i-1] += (number[i] + numOther.number[i])/10 ; }
         return result;
     }
-        
-      
-    bigNumber subtract (const bigNumber&);
+    bigNumber subtract (const bigNumber& numOther) {
+    string result="0";
+    int lngth=number.length();
+    if(numOther.number.length() > lngth)
+      lngth=numOther.number.length();
+    for(int i=length-1; i>-1; i--) {
+      if(numOther.number[i] > number[i] ) {
+        number[i]+10;
+        number[i-1]-1; }
+      result[i] = number[i] - numOther.number[i] ; }
+    return result; }
     friend bigNumber operator<< (bigNumber);
     friend bigNumber operator>> (bigNumber);
 } ;
