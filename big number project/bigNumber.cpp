34d3c3481bc3b9db37b55c81713db3e8b77f9069
zahra-shaker <zshaker2005@gmail.com>
2024-11-01 23:24:01 +0330

    create class

--- big number project/bigNumber.cpp
+++ big number project/bigNumber.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+using namespace std;
 
+class bigNumber {
+  private:
+    string number;
+  public:
+    bigNumber();
+    bigNumber(const string&);
+    bigNumber add (const bigNumber&);
+    bigNumber subtract (const bigNumber&);
+    friend bigNumber operator<< (bigNumber);
+    friend bigNumber operator>> (bigNumber);
+} ;
