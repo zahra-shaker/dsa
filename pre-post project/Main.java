83b47c43af43bb6d8046b675bf43c09579a8c0e9
zahra-shaker <zshaker2005@gmail.com>
2024-12-23 13:07:36 +0330

    add infix to postfix method

--- pre-post project/Main.java
+++ pre-post project/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
-    public class Stack{
+    public static class EmptyStackException extends RuntimeException {
+        public EmptyStackException() {
+            super();
+        }
+        public EmptyStackException(String msg) {
+            super(msg) ;
+        }
+    }
+
+    public static class FullStackException extends RuntimeException {
+        public FullStackException() {
+            super();
+        }
+        public FullStackException(String msg) {
+            super(msg);
+        }
+    }
+    public static class Stack{
         public int size = 100 ;
         private int top = -1 ;
         private char elements[] ;
@@ -31,26 +50,8 @@ public class Main {
         }
     }
 
-    public class EmptyStackException extends RuntimeException {
-        public EmptyStackException() {
-            super();
-        }
-        public EmptyStackException(String msg) {
-            super(msg) ;
-        }
-    }
-
-    public class FullStackException extends RuntimeException {
-        public FullStackException() {
-            super();
-        }
-        public FullStackException(String msg) {
-            super(msg);
-        }
-    }
-
     static boolean isOperator (char c) { 
-        return (c == '+' || c == '-' || c == '*' || c == '/' || c=='(' || c== ')' ) ;
+        return (c == '+' || c == '-' || c == '*' || c == '/' || c=='(' ) ;
     }
 
     static boolean Priority (char Cin, Character stTop) {
@@ -65,34 +66,61 @@ public class Main {
         if (Cin=='/' && stTop=='(') result=true ;
         if (Cin=='+' && stTop == null) result=true ;
         if (Cin=='-' && stTop == null) result=true ;
-        if (Cin=='*' && stTop== null) result=true ;
-        if (Cin=='/' && stTop== null) result=true ;
+        if (Cin=='*' && stTop == null) result=true ;
+        if (Cin=='/' && stTop == null) result=true ;
         return result;
     }
     static String inToPost(String Sinput) {
         int j = Sinput.length() ;
         Stack stk ;
+        stk = new Stack(j) ;
         char[] post ;
         post = new char[j] ;
-        for(int i=0; i<Sinput.length(); i++) {
+        j-- ;
+        for(int i=0; i<j; i++) {
             char in = Sinput.charAt(i) ;
-            if (Character.isDigit(in)) post[j-1] = in ; 
-            if(isOperator(in)) {
-
+            if (Character.isDigit(in)) { 
+                post[j] = in ;
+                j-- ; }
+            if (isOperator(in)) { 
+                if (Priority(in,stk.peek())) {
+                    stk.push(in);
+                }
+                if (in==')') {
+                    while (stk.peek()!='(') {
+                        post[j] = stk.pop() ;
+                        j--;
+                    }
+                    stk.pop() ;
+                }
+                if (!Priority(in,stk.peek())) {
+                    post[j] = stk.pop() ;
+                    j-- ;
+                }
             }
-
         }
-
-        
-        return "";
+        while (!stk.isEmpty()) {
+            post[j] = stk.pop() ;
+            j-- ; 
+        }
+        String result = new String(post) ;
+        return result ;
     }
+ 
     static String inToPre(String Sinput) {
+        int j = Sinput.length() ;
+        Stack stOperand = new Stack(j) ;
+        Stack stOperator = new Stack(j) ;
+        for (int i=0 ; i<Sinput.length())
+
         return "" ;
     }
     public static void main(String args[]){
-        String sin ;
-        sin = "b+a*c/d" ;
-        System.out.println(inToPost(sin));
-        System.out.println(inToPre(sin));
+        String infix ;
+        System.out.println("Enter infix form:");
+        Scanner obj = new Scanner (System.in) ; 
+        infix = obj.nextLine() ;
+        System.out.println(inToPost(infix));
+        System.out.println(inToPre(infix));
     }
 }
