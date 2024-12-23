119e323684f2b40f7de95c954a2ba4415ca94b89
zahra-shaker <zshaker2005@gmail.com>
2024-12-23 11:02:16 +0330

    creat main classes

--- pre-post project/Main.java
+++ pre-post project/Main.java
@@ -1,1 +1,14 @@
-
+public class Main {
+    static String inToPost(String Sinput) {
+        return "";
+    }
+    static String inToPre(String Sinput) {
+        return "" ;
+    }
+    public static void main(String args[]){
+        String sin ;
+        sin = "b+a*c/d" ;
+        System.out.println(inToPost(sin));
+        System.out.println(inToPre(sin));
+    }
+}
